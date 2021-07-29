b803884e7fcbc85d42287dbca631a88e89cf7e5e
e1even <Fedemito2010@gmail.com>
2021-07-29 15:42:21 +0200

    set Date

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 
 const WeatherCard = () => {
+  const dataBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} / ${month} / ${year}`;
+  };
   return (
     <>
       <div className="container_card">
         <div className="location">New York City, US</div>
-        <div className="date">11 Febbraio 2000</div>
+        <div className="date">{dataBuilder(new Date())}</div>
         <div className="cardTemperature">
           <div className="temperature">30°</div>
         </div>
